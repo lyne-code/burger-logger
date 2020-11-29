f401a4141e940ebdfae7f43269be881f2124b3c3
Lyne Malele <lynemalele@gmail.com>
2020-11-29 02:10:21 -0500

    First Commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burger (burger_name) VALUES ('Krabby Patty');
+INSERT INTO burger (burger_name) VALUES ('Baconator');
+INSERT INTO burger (burger_name, devoured) VALUES ('Triple Beef', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Chicken Lickin', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Quarter Pounder', true);
+INSERT INTO burger (burger_name) VALUES ('Little Mac');
